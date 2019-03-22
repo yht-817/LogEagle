adf00063687de7663b8a77e8d3b3db588237ff9a
yht-817 <1475409096@qq.com>
2019-03-22 16:49:45 +0800

    添加项目

---
+++ Eagle-Log/src/main/java/com/yht/EagleLog/EagleLogApplication.java
@@ -0,0 +1,13 @@
+package com.yht.EagleLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleLogApplication.class, args);
+	}
+
+}
